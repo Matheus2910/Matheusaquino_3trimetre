10d527bfdd3c6a1f5baace1fbcecdb986cc54f53
Matheus2910 <110101691+Matheus2910@users.noreply.github.com>
2023-10-04 12:26:34 -0300

    pocfchgf

---
+++ aluramidi-curso-aula3/main.js
@@ -0,0 +1,11 @@
+// Pom -----------------function tocaSomPom () {
+    function tocaSomPom () {
+        document.querySelector('#som_tecla_pom').play();
+    }
+
+
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
